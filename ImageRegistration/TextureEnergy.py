21f73a485fccfaa8a72e05f414146aae35bdf177
alearcyber <alear@cyberprotex.com>
2023-06-22 09:17:26 -0500

    Finished creating the texture kernels. Tested the kernels and saved the examples to /ImageRegistration/TetxtureMaskExamples/ for others to glance at

--- ImageRegistration/TextureEnergy.py
+++ ImageRegistration/TextureEnergy.py
@@ -1,12 +1,8 @@
-##
-# level
-#
-#
-#
-#
-#
-#
-#
+"""
+This file explores law's texture masks and kernels.
+
+TODO: finish writing out the details of this file
+"""
 
 
 import numpy as np
@@ -34,19 +30,23 @@ ls = np.dot(level.reshape(-1, 1), spot)
 lr = np.dot(level.reshape(-1, 1), ripple)
 
 #spot kernels
-#TODO FINISH BUILDINg the kernSL
+sl = np.dot(spot.reshape(-1, 1), level)
+se = np.dot(spot.reshape(-1, 1), edge)
+ss = np.dot(spot.reshape(-1, 1), spot)
+sr = np.dot(spot.reshape(-1, 1), ripple)
 
+#ripple kernels
+rl = np.dot(ripple.reshape(-1, 1), level)
+re = np.dot(ripple.reshape(-1, 1), edge)
+rs = np.dot(ripple.reshape(-1, 1), spot)
+rr = np.dot(ripple.reshape(-1, 1), ripple)
 
-
-#currently not used
-def build_kernels():
-    filters = [
-        {'name': 'level', 'vector': np.array([[1, 4, 6, 4, 1]])},
-        {'name': 'edge', 'vector': np.array([[-1, -2, 0, 2, 1]])},
-        {'name': 'spot', 'vector': np.array([-1, 0, 2, 0, -1])},
-        {'name': 'ripple', 'vector': np.array([1, -4, 6, -4, 1])}
-    ]
-
+texture_kernels = [ # list of 2-tuples: ('name', kernel)
+    ('edge x level', el), ('edge x edge', ee), ('edge x spot', es), ('edge x ripple', er),
+    ('level x level', ll), ('level x edge', le), ('level x spot', ls), ('level x ripple', lr),
+    ('spot x level', sl), ('spot x edge', se), ('spot x spot', ss), ('spot x ripple', sr),
+    ('ripple x level', rl), ('ripple x edge', re), ('ripple x spot', rs), ('ripple x ripple', rr),
+]
 
 
 
@@ -88,16 +88,24 @@ def test_removing_illumination():
     show(out, 'illumination removed')
 
     r1 = cv2.filter2D(out, ddepth=-1, kernel=el)
-    show(r1, 'mask')
+    r2 = cv2.filter2D(image, ddepth=-1, kernel=el)
+    show(r1, 'mask with illumination removed')
+    show(r2, 'mask without preprocessing')
 
 
-
-
+#Applies the 16 kernels to the image to create 16 bands of texture features,
+# (each band has 3 colors).
+#Returns the 16 images as a list.
+#Currently does not label the different bands in any significant way
+def generate_texture_features(img):
+    img = remove_illumination(img)
+    for descriptor, kernel in texture_kernels:
+        out = cv2.filter2D(img, ddepth=-1, kernel=kernel)
+        #cv2.imwrite("TextureMaskExamples/" + descriptor + ".png", out)  #this line saves the masks
+        show(out, descriptor)
 
 
 if __name__ == '__main__':
-
-
-    test_removing_illumination()
+    generate_texture_features(cv2.imread('images/fail3.jpg'))
 
 
