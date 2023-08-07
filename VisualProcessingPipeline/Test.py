49cbacfd9bd40f8eb1bd7838d292ecd35afe4aed
alearcyber <alear@cyberprotex.com>
2023-08-07 17:42:52 -0500

    Began Working on Texture Filter, need to figure out which textures are bad for this.

--- VisualProcessingPipeline/Test.py
+++ VisualProcessingPipeline/Test.py
@@ -1,5 +1,7 @@
 from BlurFilter import BlurFilter
 import cv2
+import TextureFilter
+import numpy as np
 
 
 def test1():
@@ -12,7 +14,18 @@ def test1():
     cv2.imshow('median blur 5 x 5', f2.o_filtered)
     cv2.waitKey()
 
+def test2():
+    observed = cv2.imread("/Users/aidanlear/PycharmProjects/CVResearch/ImageRegistration/images/fail3.jpg")
+    out = TextureFilter.texture_features(observed)
+
+    cv2.imshow('composite', np.mean(out, axis=2).astype(np.uint8))
+    cv2.waitKey()
+    for i in range(9):
+        o = out[:, :, i]
+        cv2.imshow('texture' + str(i), o)
+    print(out.shape)
+    cv2.waitKey()
 
 if __name__ == '__main__':
     """run tests"""
-    test1()
+    test2()
