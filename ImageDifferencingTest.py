de73d285be72f9a34785863ee97eafb49af9f81a
aidan <aidan@aidans-Mac-Studio.local>
2024-01-19 13:17:48 -0600

    pushing changes from the other day. Continue working in ImageDifferencingTest.py

---
+++ ImageDifferencingTest.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+
+def downsample(img, rows, cols):
+    """
+    Down-sample an image into rows x cols size. Maintains original image dimensions.
+    """
+    #get original image shape
+    original_shape = img.shape
+
+    # do the downsampling
+    downsampled = cv2.resize(img, (cols, rows), interpolation=cv2.INTER_AREA)
+
+    #go back up to the original shape
+    return cv2.resize(downsampled, (original_shape[1], original_shape[0]), interpolation=cv2.INTER_NEAREST_EXACT)
+
+
+def test_downsampling():
+    img = cv2.imread("/Users/aidan/PycharmProjects/DYSCO/Data/TestSet1/test1/cam-low-exposure.png")
+
+
+    area = downsample(img, 5, 10)
+    #cv2.imshow("downsampled", area)
+    #cv2.waitKey()
+
+
+if __name__ == "__main__":
+    test_downsampling()
