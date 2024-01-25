7c9e715c2ba005ea966ac5d7736e9508caf919c7
alearcyber <alear@cyberprotex.com>
2024-01-25 14:52:30 -0600

    Finished a test with canny edge detector in ImageDifferencingEdge.py, and the results can be seen in the corresponding Results directory.

--- ImageDifferencingEdge.py
+++ ImageDifferencingEdge.py
@@ -9,6 +9,8 @@ canny, sobel, laplacian, and fourier
 
 import numpy as np
 import cv2
+from sklearn.cluster import KMeans
+from Verify import *
 
 
 
@@ -24,9 +26,9 @@ def bilateral_blur_example():
 
 def bilateral_canny_example():
     test_image = cv2.imread("Data/TestSet1/test1/cam-low-exposure.png")
-
-
     cv2.imshow(f'canny, no blur', cv2.Canny(test_image, 100, 200))
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
     for i in range(3, 10, 2):
         blurred_image = cv2.bilateralFilter(test_image, i, 75, 75)
@@ -36,11 +38,99 @@ def bilateral_canny_example():
         cv2.destroyAllWindows()
 
 
+def test1():
+    """
+    This function runs the following test:
+    grayscale image, 3x3 bilateral filter(blur), canny edge, 10 row x 20 col downsample,
+    difference between observed and expected, k-means cluster.
+    """
+    #number of rows and columns working with for this experiment
+    rows = 10
+    cols = 20
+    bilateral_filter_size = 3
+
+
+    #Read the test images into memory.
+    #Each test is a dictionary inside the list, test_images.
+    dir = "Data/TestSet1"
+    test_images = []
+    for i in range(1, 9):
+        test = {
+            'observed': cv2.imread(f"{dir}/test{i}/cam-low-exposure.png"),
+            'expected': cv2.imread(f"{dir}/test{i}/expected-expanded.png"),
+            'ground_truth': cv2.imread(f"{dir}/test{i}/obstructionmask.png")
+        }
+        test_images.append(test)
+
+
+
+
+
+    # Preprocess the observed and expected images...
+    # No preprocessing, just downsample and chi squared distance
+    i = 0
+    for test in test_images:
+        i += 1
+
+        #Convert the images to grayscale
+        observed = cv2.cvtColor(test['observed'], cv2.COLOR_BGR2GRAY)
+        expected = cv2.cvtColor(test['expected'], cv2.COLOR_BGR2GRAY)
+
+
+        #bilateral filter
+        observed = cv2.bilateralFilter(observed, bilateral_filter_size, 75, 75)
+        expected = cv2.bilateralFilter(expected, bilateral_filter_size, 75, 75)
+
+        #canny
+        observed = cv2.Canny(observed, 100, 200)
+        expected = cv2.Canny(expected, 100, 200)
+
+        #downsampling
+        observed = cv2.resize(observed, (cols, rows), interpolation=cv2.INTER_AREA)
+        expected = cv2.resize(expected, (cols, rows), interpolation=cv2.INTER_AREA)
+
+        #Difference
+        difference = np.absolute(observed.astype(int) - expected.astype(int))
+
+
+
+        # Cluster the outcome
+        flat = difference.reshape(-1, 1)  # flattened difference image
+        kmeans = KMeans(n_clusters=2, random_state=0, n_init="auto").fit(flat)
+        print('labels:', kmeans.labels_)
+        print('Cluster centers:', kmeans.cluster_centers_)
+        reshaped_labels = kmeans.labels_.reshape(rows, cols)
+        print('----reshaped labels----\n', reshaped_labels)
+
+        # figure out which cluster center is the highest so I know which area is the
+        # obstructed area, and which is unobstructed. The highest centroid is the obstructed area.
+        if np.linalg.norm(kmeans.cluster_centers_[0, :]) > np.linalg.norm(kmeans.cluster_centers_[1, :]):
+            # the 0th centroid is bigger
+            prediction_matrix = (reshaped_labels == 1).tolist()
+        else:
+            # the first centroid is bigger
+            prediction_matrix = (reshaped_labels == 0).tolist()
+
+
+
+        #visualize the outcomes
+        visualized_prediction = visualize_region_matrix(test['observed'], prediction_matrix)  # visualized results
+        cv2.imshow(f'Prediction {i}', visualized_prediction)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        cv2.imwrite(f'Results/ImageDifferencingEdge-test1/Test{i}.png', visualized_prediction)
+
+
+
+
+
+
+
 
 
 
 if __name__ == "__main__":
-    bilateral_canny_example()
+    test1()
 
 
 
